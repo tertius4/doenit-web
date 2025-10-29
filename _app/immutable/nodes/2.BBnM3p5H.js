d75bd8344a61727771002c38c54d471f1baccc24
Tertius van Niekerk <tertius.vanniekerk@protonmail.com>
2025-10-29 17:59:54 +0200

    toets

---
+++ _app/immutable/nodes/2.BBnM3p5H.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/BYj3PBTR.js";import{e,b as p,n as a}from"../chunks/Bcvu3o5W.js";function i(t){a();var o=e("Hello, World!");p(t,o)}export{i as component};
