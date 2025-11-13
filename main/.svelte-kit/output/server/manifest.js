061401c011729aa436e614cd09452f53336c7aeb
Tertius van Niekerk <tertius.vanniekerk@protonmail.com>
2025-11-13 19:43:17 +0200

    Updated site content

--- main/.svelte-kit/output/server/manifest.js
+++ main/.svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["doenit-logo.webp","instellings.webp","page-not-found.webp","robots.txt","sitemap.xml","tertius_picture.webp","tuisblad.webp","wysig-taak.webp"]),
 	mimeTypes: {".webp":"image/webp",".txt":"text/plain",".xml":"text/xml"},
 	_: {
-		client: {start:"_app/immutable/entry/start.B8y6viQD.js",app:"_app/immutable/entry/app.CRWkOwMk.js",imports:["_app/immutable/entry/start.B8y6viQD.js","_app/immutable/chunks/CpfxP-z9.js","_app/immutable/chunks/DHMrLEOE.js","_app/immutable/chunks/Bvot_Dxv.js","_app/immutable/chunks/D8Z9p0WQ.js","_app/immutable/entry/app.CRWkOwMk.js","_app/immutable/chunks/Bvot_Dxv.js","_app/immutable/chunks/D8Z9p0WQ.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/DHMrLEOE.js","_app/immutable/chunks/BAF9bqbN.js","_app/immutable/chunks/LURHR_DV.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.G61n4Zni.js",app:"_app/immutable/entry/app.BxW_Q7d5.js",imports:["_app/immutable/entry/start.G61n4Zni.js","_app/immutable/chunks/CEpZF3cg.js","_app/immutable/chunks/DHMrLEOE.js","_app/immutable/chunks/Bvot_Dxv.js","_app/immutable/chunks/D8Z9p0WQ.js","_app/immutable/entry/app.BxW_Q7d5.js","_app/immutable/chunks/Bvot_Dxv.js","_app/immutable/chunks/D8Z9p0WQ.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/DHMrLEOE.js","_app/immutable/chunks/BAF9bqbN.js","_app/immutable/chunks/LURHR_DV.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js'))
--- main/.svelte-kit/output/server/nodes/3.js
+++ main/.svelte-kit/output/server/nodes/3.js
@@ -3,6 +3,6 @@
 export const index = 3;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/doenit/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.B-5LRXCR.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/e1tVdP00.js","_app/immutable/chunks/Bvot_Dxv.js","_app/immutable/chunks/Dzu54c7L.js","_app/immutable/chunks/D8Z9p0WQ.js","_app/immutable/chunks/BQOG-0MG.js","_app/immutable/chunks/CpfxP-z9.js","_app/immutable/chunks/DHMrLEOE.js"];
+export const imports = ["_app/immutable/nodes/3.BR_Zw3jG.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/e1tVdP00.js","_app/immutable/chunks/Bvot_Dxv.js","_app/immutable/chunks/Dzu54c7L.js","_app/immutable/chunks/D8Z9p0WQ.js","_app/immutable/chunks/BjWYa87s.js","_app/immutable/chunks/CEpZF3cg.js","_app/immutable/chunks/DHMrLEOE.js"];
 export const stylesheets = [];
 export const fonts = [];
